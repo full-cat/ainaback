68efb08992abc64b430acf532d21e07659597336
Enrique Molina <enrique.molina@urv.cat>
2024-11-08 22:58:35 +0100

    aina2

--- proxy.py
+++ proxy.py
@@ -2,14 +2,16 @@ from flask import Flask, request, Response
 import requests
 from bs4 import BeautifulSoup, Comment, NavigableString
 from urllib.parse import urljoin
+from urllib.parse import urlparse
 
 app = Flask(__name__)
 
 @app.route('/proxy', methods=['GET'])
 def proxy():
     # Obtener la URL de la página principal desde los parámetros
-    global url
+    global base_url
     url = request.args.get('url')
+    base_url = "https://" + urlparse(url).netloc
     if not url:
         return "URL no proporcionada", 400
     
@@ -39,30 +41,45 @@ def proxy():
             # new_text = f"+{original_text}"
             # text_element.replace_with(new_text)
 
+    # replace all links in html
+    # Find all <a> tags
+    for link in soup.find_all('a'):
+        # Replace the href attribute with a new link (for example, replace it with 'https://new-link.com')
+        if link.get('href') is None:
+            continue
+        stem = url if link.get('href').startswith("/") else ""
+        if link.get('href'):
+            link['href'] = "http://localhost:5000/proxy?url=" + stem +  link['href']
+        # Alternatively, replace the entire content of the <a> tag
+        # link.string = "New Link Text"
 
+    # Get the modified HTML content
+    modified_html = str(soup)
 
     
-    # Reescribir los recursos estáticos (CSS, imágenes, etc.) para que apunten al proxy
-    for tag in soup.find_all(['link', 'script', 'img', "spreadsheet"]):
-        if tag.name == 'link' and tag.get('href'):
-            tag['href'] = urljoin(url, tag['href'])
-        elif tag.name == 'script' and tag.get('src'):
-            tag['src'] = urljoin(url, tag['src'])
-        elif tag.name == 'img' and tag.get('src'):
-            tag['src'] = urljoin(url, tag['src'])
-        elif tag.name == 'spreadsheet' and tag.get('src'):
-            tag['src'] = urljoin(url, tag['src'])
+    # # Reescribir los recursos estáticos (CSS, imágenes, etc.) para que apunten al proxy
+    # for tag in soup.find_all(['link', 'script', 'img', "spreadsheet"]):
+    #     if tag.name == 'link' and tag.get('href'):
+    #         tag['href'] = urljoin(url, tag['href'])
+    #     elif tag.name == 'script' and tag.get('src'):
+    #         tag['src'] = urljoin(url, tag['src'])
+    #     elif tag.name == 'img' and tag.get('src'):
+    #         tag['src'] = urljoin(url, tag['src'])
+    #     elif tag.name == 'spreadsheet' and tag.get('src'):
+    #         tag['src'] = urljoin(url, tag['src'])
 
-    # Convertir el HTML modificado en cadena para enviarlo
-    modified_html = str(soup)
+    # # Convertir el HTML modificado en cadena para enviarlo
+    # modified_html = str(soup)
     return Response(modified_html, content_type='text/html')
 
-@app.route('/<path:filename>')
+# @app.route('/<path:filename>')
+# allow ALL methods
+@app.route('/<path:filename>', methods=['GET', 'POST', 'PUT', 'DELETE'])
 def serve_media(filename):
-    global url
+    global base_url
 
     # Construir la URL completa para el recurso solicitado
-    resource_url = urljoin(url, filename)
+    resource_url = urljoin(base_url, filename)
     
     # Realizar la solicitud para obtener el recurso (imagen, CSS, etc.)
     try:
