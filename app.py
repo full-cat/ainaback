af3001cda43eb501f3af2e7874e23dbd0d136259
Enrique Molina <enrique.molina@urv.cat>
2024-11-08 20:06:23 +0100

    first cp

---
+++ app.py
@@ -0,0 +1,28 @@
+from flask import Flask, request
+import requests
+
+app = Flask(__name__)
+
+brave_key = 'BSAjdpzNenLcG9UCpbQn_uP6A7yy10h'
+search_url = 'https://api.search.brave.com/res/v1/web/search'
+
+
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
+
+
+@app.route('/search')
+def search():
+    query = request.args.get('q')
+    headers = {
+        'Accept': 'application/json',
+        'Accept-Encoding': 'gzip',
+        'X-Subscription-Token': brave_key
+    }
+    response = requests.get(search_url, params={'q': query}, headers=headers)
+    return response.json()
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
